8a5dbf4550d9dbba58e0c5700932a3f934053359
Toni Anne Romero <tonianne.romero@gmail.com>
2015-11-24 17:39:24 +0800

    added port heroku

--- config/config.js
+++ config/config.js
@@ -26,7 +26,7 @@ var config = {
     app: {
       name: 'helpz'
     },
-    port: 3000,
+    port: (process.env.PORT || 3000 ),
     db: 'mongodb://heroku_585ksbg7:1f7ii1jvh02mt46ni1nrqnal3q@ds057204.mongolab.com:57204/heroku_585ksbg7'
   }
 };
