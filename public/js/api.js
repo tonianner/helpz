08b06dba437a5dbbfc972e23713d83021e08cfee
Toni Anne Romero <tonianne.romero@gmail.com>
2015-11-25 17:54:00 +0800

    show page done

--- public/js/api.js
+++ public/js/api.js
@@ -2,12 +2,22 @@
 var API_WRAPPER = function () {
   this.URL_BASE = window.location.origin
 
-  this.getSecret = function (){
+  // this.getSecret = function (){
+  //   return $.ajax({
+  //     url:    this.URL_BASE + "/secret",
+  //     method: "GET"
+  //   })
+  // }
+
+  // get all articles
+  this.getAllServices = function(){
     return $.ajax({
-      url:    this.URL_BASE + "/secret",
-      method: "GET"
-    })
+      url:  this.URL_BASE + '/api/helpz',
+      type: 'GET'
+    });
   }
 };
+
+
 
 var API = new API_WRAPPER();
--- public/js/services/index.js
+++ public/js/services/index.js
@@ -1,6 +1,54 @@
 // .then(success, error)
+// $(function(){
+//   API.getSecret().then(function(data){
+//     console.log("yeah! " + data)
+//   }, errorHandling)
+// });
+
 $(function(){
-  API.getSecret().then(function(data){
-    console.log("yeah! " + data)
-  }, errorHandling)
-});
+  API.getAllServices().then(function(services){
+    services.forEach(function(service){
+      $('#populateData').append('\
+        <div class="well populate-each">\
+          <div class="media">\
+            <div class="media-body">\
+              <h4 class="media-heading">\
+                <i class="fa fa-quote-left"></i> '+ service.title +'\
+              </h4>\
+              <ul class="list-inline pull-right">\
+                <li> <h4> hk$100 </h4></li>\
+                <li>\
+                  <button disabled="disabled" class="btn btn-info btn-lg">\
+                    Contact me \
+                    <i class="fa fa-facebook-square"></i>\
+                  </button>\
+                </li>\
+              </ul>\
+              <p> '+ service.description +'</p>\
+              <ul class="list-inline list-unstyled">\
+                <li><span><i class="glyphicon glyphicon-calendar"></i> Created on: '+ service.createdOn +' </span></li>\
+                <li>|</li>\
+                  <i class="fa fa-user"></i> by ' + service.createdBy + ' </span>\
+                <li>|</li>\
+                <li>\
+                  <i class="fa fa-clock-o"></i> ' + service.duration + ' on average</span>\
+                </li>\
+                <br>\
+                <li>\
+                  <a href= "#">\
+                    <i class="fa fa-trash"></i> Delete Entry\
+                  </a>\
+                </li>\
+                <li>\
+                  <a href="">\
+                    <i class="fa fa-pencil"></i> Edit Post\
+                  </a>\
+                </li>\
+              </ul>\
+            </div>\
+          </div>\
+        </div>\
+      ');
+    })
+  })
+})
